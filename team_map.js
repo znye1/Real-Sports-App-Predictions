403bd672f842db926ecee3a4d720d12058dec54b
znye1 <zhiyanye1@gmail.com>
2026-09-19 20:11:06 -0400

    Add new teams and update existing team mappings

--- team_map.js
+++ team_map.js
@@ -1,4 +1,7 @@
 export const TEAM_MAP = {
+    "AKR": {
+        "Akron": "Akron"
+    },
     "ALA": {
         "Alabama": "Alabama"
     },
@@ -6,7 +9,8 @@ export const TEAM_MAP = {
         "Ducks": "Anaheim Ducks"
     },
     "ARI": {
-        "Cardinals": "Arizona Cardinals"
+        "Cardinals": "Arizona Cardinals",
+        "D-backs": "Arizona Diamondbacks"
     },
     "ARIZ": {
         "Arizona": "Arizona"
@@ -15,10 +19,19 @@ export const TEAM_MAP = {
         "Arkansas": "Arkansas"
     },
     "ATL": {
+        "Braves": "Atlanta Braves",
+        "Dream": "Atlanta Dream",
         "Falcons": "Atlanta Falcons",
         "Hawks": "Atlanta Hawks"
     },
+    "AUB": {
+        "Auburn": "Auburn"
+    },
+    "AUS": {
+        "Australia": "Australia"
+    },
     "BAL": {
+        "Orioles": "Baltimore Orioles",
         "Ravens": "Baltimore Ravens"
     },
     "BKN": {
@@ -26,7 +39,11 @@ export const TEAM_MAP = {
     },
     "BOS": {
         "Bruins": "Boston Bruins",
-        "Celtics": "Boston Celtics"
+        "Celtics": "Boston Celtics",
+        "Red Sox": "Boston Red Sox"
+    },
+    "BRA": {
+        "Brazil": "Brazil"
     },
     "BUF": {
         "Bills": "Buffalo Bills",
@@ -35,6 +52,9 @@ export const TEAM_MAP = {
     "BYU": {
         "BYU": "BYU"
     },
+    "CAN": {
+        "Canada": "Canada"
+    },
     "CAR": {
         "Hurricanes": "Carolina Hurricanes",
         "Panthers": "Carolina Panthers"
@@ -42,34 +62,61 @@ export const TEAM_MAP = {
     "CBJ": {
         "Blue Jackets": "Columbus Blue Jackets"
     },
+    "CBU": {
+        "CBU": "California Baptist"
+    },
     "CGY": {
         "Flames": "Calgary Flames"
     },
     "CHA": {
         "Hornets": "Charlotte Hornets"
     },
+    "CHC": {
+        "Cubs": "Chicago Cubs"
+    },
     "CHI": {
         "Bears": "Chicago Bears",
         "Blackhawks": "Chicago Blackhawks",
-        "Bulls": "Chicago Bulls"
+        "Bulls": "Chicago Bulls",
+        "Sky": "Chicago Sky"
     },
     "CIN": {
-        "Bengals": "Cincinnati Bengals"
+        "Bengals": "Cincinnati Bengals",
+        "Reds": "Cincinnati Reds"
     },
     "CLE": {
         "Browns": "Cleveland Browns",
-        "Cavaliers": "Cleveland Cavaliers"
+        "Cavaliers": "Cleveland Cavaliers",
+        "Guardians": "Cleveland Guardians"
+    },
+    "CLEM": {
+        "Clemson": "Clemson"
     },
     "COL": {
-        "Avalanche": "Colorado Avalanche"
+        "Avalanche": "Colorado Avalanche",
+        "Colombia": "Colombia",
+        "Rockies": "Colorado Rockies"
+    },
+    "CON": {
+        "Sun": "Connecticut Sun"
     },
     "CONN": {
         "UConn": "Connecticut"
     },
+    "CUB": {
+        "Cuba": "Cuba"
+    },
+    "CWS": {
+        "White Sox": "Chicago White Sox"
+    },
+    "CZE": {
+        "Czechia": "Czechia"
+    },
     "DAL": {
         "Cowboys": "Dallas Cowboys",
         "Mavs": "Dallas Mavericks",
-        "Stars": "Dallas Stars"
+        "Stars": "Dallas Stars",
+        "Wings": "Dallas Wings"
     },
     "DEN": {
         "Broncos": "Denver Broncos",
@@ -78,7 +125,11 @@ export const TEAM_MAP = {
     "DET": {
         "Lions": "Detroit Lions",
         "Pistons": "Detroit Pistons",
-        "Red Wings": "Detroit Red Wings"
+        "Red Wings": "Detroit Red Wings",
+        "Tigers": "Detroit Tigers"
+    },
+    "DOM": {
+        "Dominican": "Dominican Republic"
     },
     "DUKE": {
         "Duke": "Duke"
@@ -86,31 +137,72 @@ export const TEAM_MAP = {
     "EDM": {
         "Oilers": "Edmonton Oilers"
     },
+    "ENG": {
+        "England": "England"
+    },
+    "FSU": {
+        "FSU": "Florida State"
+    },
     "FLA": {
         "Florida": "Florida",
         "Panthers": "Florida Panthers"
     },
+    "FUR": {
+        "Furman": "Furman"
+    },
     "GB": {
         "Packers": "Green Bay Packers"
+    },
+    "GBR": {
+        "Great Britain": "Great Britain"
+    },
+    "GONZ": {
+        "Gonzaga": "Gonzaga"
+    },
+    "GSV": {
+        "Valkyries": "Golden State Valkyries"
     },
     "GSW": {
         "Warriors": "Golden State Warriors"
     },
+    "HAW": {
+        "Hawai'i": "Hawai'i"
+    },
     "HOU": {
+        "Astros": "Houston Astros",
         "Houston": "Houston",
         "Rockets": "Houston Rockets",
         "Texans": "Houston Texans"
     },
+    "HOW": {
+        "Howard": "Howard"
+    },
+    "HP": {
+        "HP": "High Point"
+    },
+    "IDHO": {
+        "Idaho": "Idaho"
+    },
     "ILL": {
         "Illinois": "Illinois"
     },
+    "IOWA": {
+        "Iowa": "Iowa"
+    },
+    "ITA": {
+        "Italy": "Italy"
+    },
     "IND": {
         "Colts": "Indianapolis Colts",
+        "Fever": "Indiana Fever",
         "Indiana": "Indiana",
         "Pacers": "Indiana Pacers"
     },
     "IOWA": {
         "Iowa": "Iowa"
+    },
+    "ISR": {
+        "Israel": "Israel"
     },
     "ISU": {
         "ISU": "Iowa State"
@@ -118,15 +210,28 @@ export const TEAM_MAP = {
     "JAX": {
         "Jaguars": "Jacksonville Jaguars"
     },
+    "JPN": {
+        "Japan": "Japan"
+    },
     "KC": {
-        "Chiefs": "Kansas City Chiefs"
+        "Chiefs": "Kansas City Chiefs",
+        "Royals": "Kansas City Royals"
+    },
+    "KENN": {
+        "KENN": "Kennesaw State"
     },
     "KU": {
         "Kansas": "Kansas"
     },
+    "LAA": {
+        "Angels": "Los Angeles Angels"
+    },
     "LAC": {
         "Chargers": "Los Angeles Chargers",
         "Clippers": "Los Angeles Clippers"
+    },
+    "LAD": {
+        "Dodgers": "Los Angeles Dodgers"
     },
     "LAK": {
         "Kings": "Los Angeles Kings"
@@ -137,24 +242,55 @@ export const TEAM_MAP = {
     "LAR": {
         "Rams": "Los Angeles Rams"
     },
+    "LAS": {
+        "Sparks": "Los Angeles Sparks"
+    },
+    "LEH": {
+        "Lehigh": "Lehigh"
+    },
+    "LIU": {
+        "LIU": "Long Island University"
+    },
+    "LOU": {
+        "LOU": "Louisville"
+    },
     "LV": {
         "Raiders": "Las Vegas Raiders"
+    },
+    "LVA": {
+        "Aces": "Las Vegas Aces"
+    },
+    "M-OH": {
+        "M-OH": "Miami (OH)"
+    },
+    "MCI": {
+        "Man City": "Man City"
+    },
+    "MCN": {
+        "McNeese": "McNeese"
     },
     "MEM": {
         "Grizzlies": "Memphis Grizzlies"
     },
+    "MEX": {
+        "Mexico": "Mexico"
+    },
     "MIA": {
         "Dolphins": "Miami Dolphins",
         "Heat": "Miami Heat",
-        "Miami": "Miami Florida"
+        "Marlins": "Miami Marlins",
+        "Miami": "Miami"
     },
     "MICH": {
         "Michigan": "Michigan"
     },
     "MIL": {
+        "Brewers": "Milwaukee Brewers",
         "Bucks": "Milwaukee Bucks"
     },
     "MIN": {
+        "Lynx": "Minnesota Lynx",
+        "Twins": "Minnesota Twins",
         "Vikings": "Minnesota Vikings",
         "Wild": "Minnesota Wild",
         "Wolves": "Minnesota Timberwolves"
@@ -162,17 +298,35 @@ export const TEAM_MAP = {
     "MISS": {
         "Ole Miss": "Ole Miss"
     },
+    "MIZ": {
+        "Missouri": "Missouri"
+    },
     "MSU": {
         "MSU": "Michigan State"
     },
     "MTL": {
         "Canadiens": "Montreal Canadiens"
     },
+    "NCA": {
+        "Nicaragua": "Nicaragua"
+    },
+    "NCST": {
+        "NC State": "NC State"
+    },
+    "NDSU": {
+        "NDSU": "North Dakota State"
+    },
     "NE": {
         "Patriots": "New England Patriots"
     },
     "NEB": {
         "Nebraska": "Nebraska"
+    },
+    "NED": {
+        "Netherlands": "Netherlands"
+    },
+    "NFO": {
+        "Nottm Forest": "Nottm Forest"
     },
     "NJD": {
         "Devils": "New Jersey Devils"
@@ -186,6 +340,9 @@ export const TEAM_MAP = {
     "NSH": {
         "Predators": "Nashville Predators"
     },
+    "NU": {
+        "NU": "Northwestern"
+    },
     "NYG": {
         "Giants": "New York Giants"
     },
@@ -198,8 +355,20 @@ export const TEAM_MAP = {
     "NYK": {
         "Knicks": "New York Knicks"
     },
+    "NYL": {
+        "Liberty": "New York Liberty"
+    },
+    "NYM": {
+        "Mets": "New York Mets"
+    },
     "NYR": {
         "Rangers": "New York Rangers"
+    },
+    "NYY": {
+        "Yankees": "New York Yankees"
+    },
+    "OAK": {
+        "Athletics": "Athletics"
     },
     "OKC": {
         "Thunder": "Oklahoma City Thunder"
@@ -219,20 +388,45 @@ export const TEAM_MAP = {
     "OU": {
         "Oklahoma": "Oklahoma"
     },
+    "PAN": {
+        "Panama": "Panama"
+    },
+    "PENN": {
+        "PENN": "Pennsylvania"
+    },
     "PHI": {
         "76ers": "Philadelphia 76ers",
         "Eagles": "Philadelphia Eagles",
-        "Flyers": "Philadelphia Flyers"
+        "Flyers": "Philadelphia Flyers",
+        "Phillies": "Philadelphia Phillies"
     },
     "PIT": {
         "Penguins": "Pittsburgh Penguins",
+        "Pirates": "Pittsburgh Pirates",
         "Steelers": "Pittsburgh Steelers"
+    },
+    "PHO": {
+        "Mercury": "Phoenix Mercury"
     },
     "PHX": {
         "Suns": "Phoenix Suns"
     },
     "POR": {
-        "Blazers": "Portland Trail Blazers"
+        "Blazers": "Portland Trail Blazers",
+        "Fire": "Portland Fire"
+    },
+    "PROV": {
+        "PROV": "Providence"
+    },
+    "PUR": {
+        "Puerto Rico": "Puerto Rico",
+        "Purdue": "Purdue"
+    },
+    "PV": {
+        "PV": "Prairie View A&M"
+    },
+    "QUOC": {
+        "QUOC": "Queen's University"
     },
     "SAC": {
         "Kings": "Sacramento Kings"
@@ -240,12 +434,24 @@ export const TEAM_MAP = {
     "SAS": {
         "Spurs": "San Antonio Spurs"
     },
+    "SCU": {
+        "SCU": "Santa Clara"
+    },
+    "SD": {
+        "Padres": "San Diego Padres",
+    },
     "SEA": {
         "Kraken": "Seattle Kraken",
-        "Seahawks": "Seattle Seahawks"
+        "Mariners": "Seattle Mariners",
+        "Seahawks": "Seattle Seahawks",
+        "Storm": "Seattle Storm"
     },
     "SF": {
-        "49ers": "San Francisco 49ers"
+        "49ers": "San Francisco 49ers",
+        "Giants": "San Francisco Giants"
+    },
+    "SIE": {
+        "Siena": "Siena"
     },
     "SJS": {
         "Sharks": "San Jose Sharks"
@@ -253,23 +459,40 @@ export const TEAM_MAP = {
     "SJU": {
         "SJU": "St. John's"
     },
+    "SLU": {
+        "SLU": "Saint Louis"
+    },
+    "SMC": {
+        "SMC": "Saint Mary's"
+    },
+    "SMU": {
+        "SMU": "SMU"
+    },
     "STA": {
         "Stars": "USA Stars"
     },
     "STL": {
-        "Blues": "St. Louis Blues"
+        "Blues": "St. Louis Blues",
+        "Cardinals": "St. Louis Cardinals"
     },
     "STR": {
         "Stripes": "USA Stripes"
+    },
+    "SUI": {
+        "Switzerland": "Switzerland"
     },
     "TA&M": {
         "Texas A&M": "Texas A&M"
     },
     "TB": {
-        "Buccaneers": "Tampa Bay Buccaneers"
+        "Buccaneers": "Tampa Bay Buccaneers",
+        "Rays": "Tampa Bay Rays"
     },
     "TBL": {
         "Lightning": "Tampa Bay Lightning"
+    },
+    "TCU": {
+        "TCU": "TCU"
     },
     "TEN": {
         "Titans": "Tennessee Titans"
@@ -277,34 +500,78 @@ export const TEAM_MAP = {
     "TENN": {
         "Tennessee": "Tennessee"
     },
+    "TEX": {
+        "Rangers": "Texas Rangers",
+        "Texas": "Texas"
+    },
     "TOR": {
+        "Blue Jays": "Toronto Blue Jays",
         "Maple Leafs": "Toronto Maple Leafs",
-        "Raptors": "Toronto Raptors"
+        "Raptors": "Toronto Raptors",
+        "Tempo": "Toronto Tempo"
+    },
+    "TPE": {
+        "Taipei": "Chinese Taipei"
     },
     "TTU": {
         "TTU": "Texas Tech"
     },
+    "UCF": {
+        "UCF": "UCF"
+    },
+    "UCLA": {
+        "UCLA": "UCLA"
+    },
+    "UGA": {
+        "Georgia": "Georgia"
+    },
     "UK": {
         "Kentucky": "Kentucky"
+    },
+    "UMBC": {
+        "UMBC": "UMBC"
     },
     "UNC": {
         "UNC": "North Carolina"
     },
-    "UGA": {
-        "Georgia": "Georgia"
+    "UNI": {
+        "UNI": "Northern Iowa"
+    },
+    "USA": {
+        "USA": "United States"
+    },
+    "USF": {
+        "USF": "South Florida"
+    },
+    "USU": {
+        "USU": "Utah State"
     },
     "UTA": {
         "Jazz": "Utah Jazz",
         "Mammoth": "Utah Mammoth"
     },
+    "UVA": {
+        "Virginia": "Virginia"
+    },
     "VAN": {
-        "Canucks": "Vancouver Canucks"
+        "Canucks": "Vancouver Canucks",
+        "VAN": "Vanderbilt"
+    },
+    "VCU": {
+        "VCU": "VCU"
+    },
+    "VEN": {
+        "Venezuela": "Venezuela"
     },
     "VGK": {
         "Golden Knights": "Vegas Golden Knights"
     },
+    "VILL": {
+        "Villanova": "Villanova"
+    },
     "WAS": {
         "Commanders": "Washington Commanders",
+        "Mystics": "Washington Mystics",
         "Wizards": "Washington Wizards"
     },
     "WISC": {
@@ -316,7 +583,14 @@ export const TEAM_MAP = {
     "WPG": {
         "Jets": "Winnipeg Jets"
     },
+    "WRST": {
+        "WRST": "Wright State"
+    },
     "WSH": {
-        "Capitals": "Washington Capitals"
+        "Capitals": "Washington Capitals",
+        "Nationals": "Washington Nationals"
+    },
+    "XAV": {
+        "Xavier": "Xavier"
     }
 };
